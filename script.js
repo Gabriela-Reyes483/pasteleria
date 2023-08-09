ac396b073bfec5a60f44b5570aa3ffd141eb5876
Gabriela-Reyes483 <gab.reyes483@gmail.com>
2023-08-08 23:39:58 -0600

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+window.onload = function() {
+    var nombre = prompt("Por favor, ingresa tu nombre:");
+        
+    if (nombre !== null && nombre !== "") {
+        alert("¡Hola, " + nombre + "! Bienvenido a nuestra página web.");
+    } else {
+        alert("No ingresaste un nombre. Refresca la página para intentarlo de nuevo.");
+    }
+};
