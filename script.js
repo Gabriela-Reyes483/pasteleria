60102faa300c4d933e48d2f81715fdff25eb261a
Gabriela-Reyes483 <gab.reyes483@gmail.com>
2023-08-10 21:54:31 -0600

    <Formulario y iframe>

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ window.onload = function() {
         alert("No ingresaste un nombre. Refresca la página para intentarlo de nuevo.");
     }
 };
+
+$(document).ready(function() {
+    $("#formulario").submit(function(event) {
+        event.preventDefault();
+
+        var nombre = $("#nombre").val();
+        var correo = $("#correo").val();
+        var telefono = $("#telefono").val();
+        var requisicion = $("#requisicion").val();
+
+        // Aquí puedes hacer lo que desees con los datos capturados.
+        console.log("Nombre: " + nombre);
+        console.log("Correo Electrónico: " + correo);
+        console.log("Número Telefónico: " + telefono);
+        console.log("Requisición: " + requisicion);
+    });
+});
